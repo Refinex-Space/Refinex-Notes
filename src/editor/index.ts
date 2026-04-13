f990d8dac3b15168ff256c1000eae5f958e040a0
refinex <refinexcn@gmail.com>
2026-04-13 23:12:42 +0800

    feat(editor): add src/editor/index.ts public API exports
    refs plan: 2026-04-13-refinex-editor-prosemirror-view step 3

---
+++ src/editor/index.ts
@@ -0,0 +1,5 @@
+export { RefinexEditor } from "./RefinexEditor";
+export type { RefinexEditorProps } from "./RefinexEditor";
+export { parseMarkdown, refinexParser } from "./parser";
+export { serializeMarkdown, refinexSerializer } from "./serializer";
+export { refinexSchema } from "./schema";
